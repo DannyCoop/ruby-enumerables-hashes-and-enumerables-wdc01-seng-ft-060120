44a9203453c62c8c3679a1f24663ae67b53a032e
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-05-23 02:32:33 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,9 +8,10 @@
 
 def select_winner(passengers)
   winner = ""
-  select_winner.each do |idx, name|
-    if idx == :suite_a && name.start_with?("A")
-      winner = name
-    end
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
+ 
 winner
